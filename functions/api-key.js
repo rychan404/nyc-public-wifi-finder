77c2546b5717699d98c2faab6c908b511a56a71c
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-08-05 18:42:08 -0400

    Test secret key (add request.json)

--- functions/api-key.js
+++ functions/api-key.js
@@ -1,17 +1,17 @@
 export async function onRequest({ request, env }) {
-  fetch(`https://api.geocod.io/v1.9/geocode?q=${encodeURIComponent(formattedAddress)}&api_key=${env.GEOCODIO_API_KEY}`)
+  const address = await request.json();
+  fetch(`https://api.geocod.io/v1.9/geocode?q=${encodeURIComponent(address)}&api_key=${env.GEOCODIO_API_KEY}`)
     .then(response => response.json())
     .then(data => {
       const results = data.results;
       if (results.length) {
         const { lat, lng } = results[0].location;
-        console.log(`${lat}, ${lng}`);
+        return new Response(JSON.stringify({ lat, lng }), {
+          headers: { 'Content-Type': 'application/json' },
+        });
       } else {
         console.log('No location results found');
       }
     })
     .catch(error => console.error(error));
-  return new Response(JSON.stringify({ secretUsed: true }), {
-    headers: { 'Content-Type': 'application/json' },
-  });
 }
