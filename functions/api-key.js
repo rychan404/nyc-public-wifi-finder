7a8102d9a400e0698b3828141f951ec4ccd933f5
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-08-05 18:33:42 -0400

    Test if secret key works

--- functions/api-key.js
+++ functions/api-key.js
@@ -1,8 +1,16 @@
 export async function onRequest({ request, env }) {
-  // Use secret here safely
-  const secret = env.GEOCODIO_API_KEY;
-
-  // Your logic here
+  fetch(`https://api.geocod.io/v1.9/geocode?q=${encodeURIComponent(formattedAddress)}&api_key=${env.GEOCODIO_API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      const results = data.results;
+      if (results.length) {
+        const { lat, lng } = results[0].location;
+        console.log(`${lat}, ${lng}`);
+      } else {
+        console.log('No location results found');
+      }
+    })
+    .catch(error => console.error(error));
   return new Response(JSON.stringify({ secretUsed: true }), {
     headers: { 'Content-Type': 'application/json' },
   });
--- script.js
+++ script.js
@@ -8,12 +8,6 @@ let address = {
     zip : ''
 }
 
-fetch('/api-key')
-  .then(res => res.json())
-  .then(data => {
-    console.log('Response from backend:', data);
-});
-
 myForm.addEventListener('submit', function(event) {
     event.preventDefault();
     getAddress();
@@ -31,16 +25,17 @@ function formatAddress() {
   return `${address.street}, ${address.city}, ${address.state} ${address.zip}`;
 }
 function geocode(formattedAddress) {
-  fetch(`https://api.geocod.io/v1.9/geocode?q=${encodeURIComponent(formattedAddress)}&api_key=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-      const results = data.results;
-      if (results.length) {
-        const { lat, lng } = results[0].location;
-        console.log(`${lat}, ${lng}`);
-      } else {
-        console.log('No location results found');
-      }
+  fetch('/api-key', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      address : formattedAddress
     })
-    .catch(error => console.error(error));
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log('Response from backend:', data);
+  });
 }
