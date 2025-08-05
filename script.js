dd3629d54d487cbab1e69f0365e445dbe5eb150b
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-08-05 16:34:43 -0400

    Finished geocoding user-inputted locations w/ Geocodio API

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict"
+const myForm = document.getElementById('form');
+ 
+let address = {
+    street : '',
+    city : '',
+    state : '',
+    zip : ''
+}
+
+myForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    getAddress();
+    let formattedAddress = formatAddress();
+    geocode(formattedAddress);
+});
+function getAddress() {
+  for (const addressPart of myForm.elements) {
+      if (addressPart.type === 'text' || addressPart.id === 'state') {
+          address[addressPart.id] = addressPart.value;
+      }
+  }
+}
+function formatAddress() {
+  return `${address.street}, ${address.city}, ${address.state} ${address.zip}`;
+}
+function geocode(formattedAddress) {
+  fetch(`https://api.geocod.io/v1.9/geocode?q=${encodeURIComponent(formattedAddress)}&api_key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      const results = data.results;
+      if (results.length) {
+        const { lat, lng } = results[0].location;
+        console.log(`${lat}, ${lng}`);
+      } else {
+        console.log('No location results found');
+      }
+    })
+    .catch(error => console.error(error));
+}
