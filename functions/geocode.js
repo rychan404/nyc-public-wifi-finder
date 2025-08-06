f6b9abccb5fd06b4766a951f211fe8a332c54c89
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-08-05 21:23:10 -0400

    Testing functions

--- functions/geocode.js
+++ functions/geocode.js
@@ -1,24 +1,7 @@
-export async function onRequest(context) {
-  const { request, env } = context;
-
-  const body = await request.json();
-  const address = body.formattedAddress;
-
-  const url = `https://api.geocod.io/v1.9/geocode?q=${encodeURIComponent(address)}&api_key=${env.GEOCODIO_API_KEY}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  const results = data.results;
-  if (results.length) {
-    const { lat, lng } = results[0].location;
-    return new Response(JSON.stringify(
-    {
-        latitude : lat,
-        longitude : lng
-    }
-    ), {
-        headers: { 'Content-Type': 'application/json' },
+export async function onRequestGet(context) {
+  const secretValue = context.env.GEOCODIO_API_KEY;
+  console.log(secretValue);
+  return new Response(JSON.stringify('chicken butt'), {
+     headers: { 'Content-Type': 'application/json' },
     });
-} else {
-    console.log('No location results found');
-  }
 }
--- public/script.js
+++ public/script.js
@@ -11,7 +11,12 @@ myForm.addEventListener('submit', function(event) {
     event.preventDefault();
     getAddress();
     let formattedAddress = formatAddress();
-    geocode(formattedAddress);
+    console.log(formattedAddress);
+    fetch('/geocode')
+    .then(res => res.json())
+    .then(data => {
+      console.log('Geocode result:', data);
+     });
 });
 function getAddress() {
   for (const addressPart of myForm.elements) {
@@ -24,13 +29,16 @@ function formatAddress() {
   return `${address.street}, ${address.city}, ${address.state} ${address.zip}`;
 }
 function geocode(formattedAddress) {
-  fetch('/geocode', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ formattedAddress })
-  })
-  .then(res => res.json())
-  .then(data => {
-    console.log('Geocode result:', data);
-  });
+  fetch(`https://api.geocod.io/v1.9/geocode?q=${encodeURIComponent(formattedAddress)}&api_key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      const results = data.results;
+      if (results.length) {
+        const { lat, lng } = results[0].location;
+        console.log(`${lat}, ${lng}`);
+      } else {
+        console.log('No location results found');
+      }
+    })
+    .catch(error => console.error(error));
 }
